dd10deb9080cde54e5991b75f8f80d9d3de9c32a
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-28 10:29:45 -0700

    Adds routes for users, conversations, messages via resources to routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :user_name, presence: true, uniqueness: true
+
+  # TODO: Add validations and validation range for conversations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users
+  resources :conversations do
+    resources :messages
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
