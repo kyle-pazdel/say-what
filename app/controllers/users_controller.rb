3e7422e3d4538ca0f7fb99187ba5a8fe84782b6b
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-28 10:38:34 -0700

    Adds new and create methods to users controller, and adds user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render template: "users/new"
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      render :new, message: {"There was an error, please login again."}, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
