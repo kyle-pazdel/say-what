b038a487733de44f4c70d8240115dc1f378e8cd0
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-28 11:17:51 -0700

    Adds indes, new, and create methods to messages_controller and private methods to set_conversation and message_params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,37 @@
 class MessagesController < ApplicationController
+  before_action do 
+    set_conversation
+  end
+
+  def index
+    @messages = @conversation.messages
+
+    render template: conversation_messages_path
+  end
+    
+  def new
+    @message = @conversation.messages.new
+  end
+
+  def create
+    @message = @conversation.messages.new(message_params)
+    if @message.save
+      format.html { redirect_to conversation_messages_path, notice: "Mesage sent!" }
+      format.json { render :show, status: :created, location: @message }
+    else
+      format.html { render :new, notice: "Message failed to send", status: :unprocessable_entity }
+      format.json { render json: @message.errors, status: :unprocessable_entity }
+    end
+  end
+
+
+
+    private
+      def set_conversation
+        Conversation.find(params[:conversation_id])
+      end
+
+      def message_params
+        require(:message).permit(:body, :user_id)
+      end
 end
