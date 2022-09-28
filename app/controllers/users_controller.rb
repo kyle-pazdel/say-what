e0c60b5a837215509a100a9ef59fac8226a1b1c6
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-28 10:52:35 -0700

    Adds users _form partial and new.html.erb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to "/"
-    else
-      render :new, message: {"There was an error, please login again."}, status: :unprocessable_entity
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to "/", notice: "User was successfully created." }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, notice: "There was an error, please login again.", status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
