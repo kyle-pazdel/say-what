897ded4b97174f43e6ca0b3b8f4a6653058a4f7d
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-28 10:33:45 -0700

    Corrects user routes and adds session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users
+
   resources :conversations do
     resources :messages
   end
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # User Routes
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  # Sessions Routes
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
 end
