08ea30548f68f39066154dbdb298879d7d567817
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-09-29 13:08:31 -0700

    Renders conversations and selected conversation on conversation index page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,6 +4,19 @@ class ConversationsController < ApplicationController
   def index
     @users = User.all
     @conversations = Conversation.all
+    @conversation = Conversation.find_by(id: params[:id])
+  end
+
+  def show 
+    @conversation = Conversation.find(params[:id])
+    
+    unless !@conversation.messages.last 
+      if @conversation.messages.last.user_id != current_user.id
+        @conversation.messages.last.update(read: true)
+      end
+    end
+    @message = @conversation.messages.new
+
   end
 
 
@@ -13,7 +26,10 @@ class ConversationsController < ApplicationController
     else
       @conversation = Conversation.create!(conversation_params)
     end
-     redirect_to conversation_messages_path(@conversation)
+    # if !@conversation.messages.first
+    #     @conversation.messages.create(body: "Init", user_id: params[:sender_id])
+    # end
+     redirect_to controller: "conversations", action: "create", id: @conversation.id
   end
 
 
@@ -21,4 +37,8 @@ class ConversationsController < ApplicationController
     def conversation_params
       params.permit(:sender_id, :recipient_id)
     end
+
+    # def set_conversation
+    #   Conversation.find_by(id: 1)
+    # end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,7 +21,7 @@ class MessagesController < ApplicationController
   def create
     @message = @conversation.messages.new(message_params)
     if @message.save
-      redirect_to conversation_messages_path(@conversation)
+      redirect_back(fallback_location: conversations_path)
     end
   end
 
