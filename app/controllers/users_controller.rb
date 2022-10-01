5d5d7b98becefe60bd26147526ecc589aed028f8
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-01 13:50:58 -0700

    Fixes Navbar formatting issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
   def show
-    
+    @user = User.find(params[:id])
   end
-  
+
   def index
     @users = User.all
   end
-  
+
   def new
     @user = User.new
     render template: "users/new"
@@ -54,12 +54,12 @@ class UsersController < ApplicationController
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+  end
 end
-
--- app/javascript/autohide.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-  $('.hover').on('mouseenter mouseleave', function() {
-    $('nav').toggleClass('toggleNav');
-  });
-});
