4a7e79e58ffded2e3860cb511f83a1b8d6626ba2
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-02 14:16:17 -0700

    Beginning of turbo stream implementation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,12 +4,9 @@ class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
 
   validates_uniqueness_of :sender_id, :scope => :recipient_id
+  after_create_commit { broadcast_append_to "rooms" }
 
-    def between(sender_id, recipient_id)
-      where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id, recipient_id, recipient_id, sender_id)
-    end
-
-    scope :between, -> (sender_id,recipient_id) do
-      where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
-    end
+  scope :between, ->(sender_id, recipient_id) do
+          where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id, recipient_id, recipient_id, sender_id)
+        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   include ImageUploader::Attachment(:image)
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
+  after_create_commit {broadcast_append_to "users"}
 
   # TODO: Add validations and validation range for conversations
 end
