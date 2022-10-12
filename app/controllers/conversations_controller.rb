d0cfbd7c6e71d8cfa323ce26a5f210b8bc12b394
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-10-12 16:47:51 -0700

    Adds map in conversations controller to only display conversations with messages in inbox which fixes sort bug by preventing nil errors in sort

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,11 +3,8 @@ class ConversationsController < ApplicationController
 
   def index
     @users = User.all
-    @conversations = Conversation.all.sort_by { |conversation| conversation.messages.last.created_at }
-    # @conversations = conversations_sort(@conversations)
-    # WORKING ON SORT ALGORITHM
-    ## Having an issue sorting conversations if the conversation does not have any exiting messages.
-
+    @conversations = Conversation.all.map { |conversation| conversation if conversation&.messages&.last }
+    @conversations = @conversations.sort_by { |conversation| conversation&.messages&.last&.created_at.to_i }
     @conversation = Conversation.find_by(id: params[:id])
     unless !@conversation&.messages&.last
       if @conversation.messages.last.user_id != current_user.id
@@ -33,7 +30,7 @@ class ConversationsController < ApplicationController
     else
       @conversation = Conversation.create!(conversation_params)
     end
-    redirect_to controller: "conversations", action: "create", id: @conversation.id
+    redirect_to controller: "conversations", action: "index", id: @conversation.id
   end
 
   def destroy
@@ -46,33 +43,5 @@ class ConversationsController < ApplicationController
 
   def conversation_params
     params.permit(:sender_id, :recipient_id)
-  end
-
-  def conversations_sort(conversations)
-    n = conversations.length - 1
-    n.times do |i|
-      min_index = i
-      for j in (i + 1)..n
-        min_index = j if conversations[j].messages.last.created_at > conversations[min_index].messages.last.created_at
-      end
-      conversations[i], conversations[min_index] = conversations[min_index], conversations[i] if min_index
-    end
-    conversations
-  end
-
-  def bubble_sort(conversations)
-    return conversations if conversations.size <= 1
-    swapped = true
-    while swapped
-      swapped = false
-      0.upto(conversations.size - 2) do |i|
-        if conversations.messages.last.created_at[i] > conversations.messages.last.created_at[i + 1]
-          conversations[i], conversations[i + 1] = conversations[i + 1], conversations[i]
-          swapped = true
-        end
-      end
-    end
-
-    conversations
   end
 end
